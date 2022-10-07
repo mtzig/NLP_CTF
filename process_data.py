69583c68e9384c361e0adef04b9c2cd7d818006f
Thomas Zeng <tomzeng200@gmail.com>
2022-10-07 14:00:06 -0500

    changes format for data to be added

--- process_data.py
+++ process_data.py
@@ -8,33 +8,33 @@ from datasets import SimpleDataset
 
 
 
-def get_jigsaw_datasets(file_path='./data', device='cpu', data_set = 'None'):
+def get_jigsaw_datasets(file_path='./data', device='cpu', data_type='baseline'):
 
     
     # Create df with train data
+    df_train = pd.read_csv(f'{file_path}/train_df_blind.csv')
+
+
+    if data_type == 'blind':
+        df_train = None
+    elif data_type == 'augment':
+        df_train = None
+
     
-    # Create df with test data
-    df_test = pd.read_csv(f'{file_path}/jigsaw/test.csv')
-
-
-    if data_set == 'blind':
-        df_train = pd.read_csv(f'{file_path}/train_df_blind.csv')
-    elif data_set == 'augment':
-        df_train = pd.read_csv(f'{file_path}/train_df_synthetic.csv')
-    else:
-        df_train = pd.read_csv(f'{file_path}/jigsaw/train.csv')
+    # Create df with dev data
+    df_dev = pd.read_csv(f'{file_path}/jigsaw/test.csv')
         
-    df_test_labels = pd.read_csv(f'{file_path}/jigsaw/test_labels.csv')
-    df_test['toxic'] = df_test_labels['toxic']
-    df_test = df_test[df_test['toxic'] != -1]
-    df_test.reset_index(inplace=True)
+    df_dev_labels = pd.read_csv(f'{file_path}/jigsaw/test_labels.csv')
+    df_dev['toxic'] = df_dev_labels['toxic']
+    df_dev = df_dev[df_dev['toxic'] != -1]
+    df_dev.reset_index(inplace=True)
 
-    # ints = []
+
     embed_lookup = init_embed_lookup()
      
     datasets = []
 
-    for df in (df_train, df_test):
+    for df in (df_train, df_dev):
 
         padded_id = []
         for comment in df['comment_text']:
