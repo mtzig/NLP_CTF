e28dce7f07d2493678c01909b9ffb668440d2c7d
Thomas Zeng <tomzeng200@gmail.com>
2022-10-27 11:20:47 -0500

    adds comments for how to implement csv process script

---
+++ scripts/convert_csv.py
@@ -0,0 +1,5 @@
+# Randomly splits identities to train eval split (make sure the bigrams are in the eval split)
+
+# generate civil_test_data, civi_toxic_train_data, civil_train_data, synthetic_nontoxic_df, synthetic_toxic_df (and places them in the correct directory)
+
+# also save the train identity terms in a csv file
